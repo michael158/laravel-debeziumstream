eedf5cded07c1439891038252698b36b6ada1576
Michael Douglas <michael.dsa41@gmail.com>
2018-08-23 17:40:39 -0300

    algumas alteracoes na biblioteca

--- Services/KAFKA/KafkaService.php
+++ Services/KAFKA/KafkaService.php
@@ -18,18 +18,25 @@ class KafkaService
     {
         $this->kafkaHandlerService = app()->make(KafkaHandlerService::class);
 
-        $this->kafkaServer =  env('KAFKA_HOST', '127.0.0.1'). ':' . env('KAFKA_PORT', '9092');
+        $this->kafkaServer = env('KAFKA_HOST', '127.0.0.1') . ':' . env('KAFKA_PORT', '9092');
         $this->kafkaHost = env('KAFKA_HOST', '127.0.0.1');
         $this->binaryPath = env('KAFKA_CONSUMER_BINARY_PATH', '');
         $this->binary = env('KAFKA_CONSUMER_BINARY', 'kafka-console-consumdasder');
-        $this->topic = is_null($topic)
-            ? env('DEBEZIUM_DB_SERVER_NAME') . '.' . env('DEBEZIUM_DB_DATABASE') . '.' . env('DEBEZIUM_DB_STREAMING_TABLE')
-            : $topic;
+        $this->topic = $topic;
+
     }
 
     public function consumeTopics()
     {
-        $process = new Process($this->binaryPath. $this->binary .' --bootstrap-server '. $this->kafkaServer .' --property schema.registry.url=http://'. $this->kafkaHost .':8081 --topic ' . $this->topic);
+        if(empty($this->topic))
+            throw new \Exception('please specify a topic to consumer with --topic options');
+
+        $this->handleConsumer();
+    }
+
+    protected function handleConsumer()
+    {
+        $process = new Process($this->binaryPath . $this->binary . ' --bootstrap-server ' . $this->kafkaServer . ' --property schema.registry.url=http://' . $this->kafkaHost . ':8081 --topic ' . $this->topic);
         $process->setTimeout(0);
 
         $process->start();
@@ -46,16 +53,16 @@ class KafkaService
 
     public function getTopics()
     {
-        $process = new Process($this->binaryPath. 'kafka-topics --list --zookeeper '. $this->kafkaHost .':2181');
+        $process = new Process($this->binaryPath . 'kafka-topics --list --zookeeper ' . $this->kafkaHost . ':2181');
         $process->start();
         $topics = [];
 
-        $i=0;
+        $i = 0;
         foreach ($process as $type => $message) {
             if ($process::OUT === $type) {
-                if($message != "\n"){
-                     $topics[$i]['topic_name'] = trim(preg_replace('/\s+/', ' ', $message));
-                      $i++;
+                if ($message != "\n") {
+                    $topics[$i]['topic_name'] = trim(preg_replace('/\s+/', ' ', $message));
+                    $i++;
                 }
             } else { // $process::ERR === $type
                 echo "\nRead from stderr: " . $message;
@@ -65,5 +72,17 @@ class KafkaService
         return $topics;
     }
 
+    private function getTablesForStreaming()
+    {
+        $topics = [];
 
+        $tables = explode(',', env('DEBEZIUM_DB_STREAMING_TABLE'));
+
+        foreach ($tables as $table) {
+            $topic = env('DEBEZIUM_DB_SERVER_NAME') . '.' . env('DEBEZIUM_DB_DATABASE') . '.' . $table;
+            array_push($topics, $topic);
+        }
+
+        return $topics;
+    }
 }
